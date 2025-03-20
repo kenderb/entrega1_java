6b58f2e2dd886006799071f70519baffe801f67d
kenderb <kenderbolivar@gmail.com>
2025-03-20 10:22:14 -0500

    Agrega comentarios

--- AgeCalculator.java
+++ AgeCalculator.java
@@ -6,28 +6,34 @@ import java.util.Scanner;
 
 public class AgeCalculator {
     public static void main(String[] args) {
+        // Se solicita el año al usuario
         int year = scanYear();
 
-      if (year < 1) {
-        System.out.println("El año no es valido..");
-        return;
-      }
+        // Se valida que el año sea mayor a 0
+        if (year < 1) {
+            System.out.println("El año no es valido..");
+            return;
+        }
 
-      int numero_de_siglo = siglo( year );
-      int numero_primer_anho = primerAnho( numero_de_siglo );
+        int numero_de_siglo = siglo( year );
+        int numero_primer_anho = primerAnho( numero_de_siglo );
 
-      System.out.println( "El numero del siglo es: " + numero_de_siglo );
-      System.out.println( "El numero del primer año es: " + numero_primer_anho );
+        //  Se imprimen los resultados
+        System.out.println( "El numero del siglo es: " + numero_de_siglo );
+        System.out.println( "El numero del primer año es: " + numero_primer_anho );
     }
     
+    // Se calcula el siglo
     public static int siglo( int year ) {
         return (int)  Math.ceil(year /100.0);
     }
     
+    // Se calcula el primer año del siglo
     public static int primerAnho( int siglo ) {
         return (siglo - 1) * 100 + 1;
     }
 
+    // Se solicita el año al usuario
     public static int scanYear() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el año: ");
